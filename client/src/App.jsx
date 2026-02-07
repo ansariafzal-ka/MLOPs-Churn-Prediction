cf77e3e2e65ff1f62954ce48f1cef8a1f9d35728
Ansari Mohammed Afzal <ansariafzalka@gmail.com>
2026-02-07 16:36:45 +0530

    Frontend Started

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl font-bold text-green-600">BISMILLAH</h1>
+        <p className="text-amber-100">This is a test description</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
